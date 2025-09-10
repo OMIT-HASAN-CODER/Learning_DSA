ce5ddab88b8f28c17512a72288dcdf29c1101aee
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-10 23:55:59 +0600

    area of circle second commit

--- src/Basics/AreaOfCircle.java
+++ src/Basics/AreaOfCircle.java
@@ -1,9 +1,13 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class AreaOfCircle {
     public static void main(String[] args) {
-        double r = 3.8;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        double r = input.nextDouble();
         double a = 3.141592 * r * r;
-        System.out.println(a);
+        System.out.println("\nArea of Circle is: " + a);
     }
 }
