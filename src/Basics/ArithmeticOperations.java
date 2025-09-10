18eeec59766b46f71f027ac96a3a13fb05a04ee3
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-10 21:11:36 +0600

    arithmetic operations first commit

---
+++ src/Basics/ArithmeticOperations.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int x = 5, y = 2;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        double a = 39;
+        double b = 10;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
