f2882b094feb131ef58455ce8576db6d4b571fe7
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-10 20:57:34 +0600

    variables basics first commit

---
+++ src/Basics/VariablesBasics.java
@@ -0,0 +1,23 @@
+package Basics;
+
+public class VariablesBasics {
+    public static void main(String[] args) {
+        int x = 7;
+        System.out.println(x);
+        x = x - 2;
+        System.out.println(x);
+        x = x + 20;
+        System.out.println(x);
+        x = x - 30;
+        System.out.println(x);
+        x = x * 10;
+        System.out.println(x);
+        x = x / 25;
+        System.out.println(x);
+        int y;
+        y = 20;
+        System.out.println(y);
+        y = y - x;
+        System.out.println(y);
+    }
+}
