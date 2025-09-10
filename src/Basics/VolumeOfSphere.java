49143e363ba8bfd5dce0bb26a3fb57083f6fbdfd
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-10 21:40:50 +0600

    volume of sphere first commit

---
+++ src/Basics/VolumeOfSphere.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class VolumeOfSphere {
+    public static void main(String[] args) {
+        double r = 3.8;
+        double v = (double) 4 /3 * 3.141592 * r * r * r;
+        System.out.println(v);
+    }
+}
