9e53394d86772e2d72efd0a0a946735e21210fc5
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-11 02:42:27 +0600

    relational operators first commit

---
+++ src/IfElse/RelationalOperators.java
@@ -0,0 +1,13 @@
+package IfElse;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        System.out.println(5 > 3);
+        System.out.println(5 >= 3);
+        System.out.println(5 < 3);
+        System.out.println(5 <= 3);
+        System.out.println(5 == 3);
+        System.out.println(5 != 3);
+
+    }
+}
