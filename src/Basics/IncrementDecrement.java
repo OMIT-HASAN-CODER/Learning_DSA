54d3a3d6991bc0d6c95854f8486418ba92682322
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-11 02:21:38 +0600

    increment decrement first commit

---
+++ src/Basics/IncrementDecrement.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x++;
+        System.out.println(x + " " + y);
+        y = ++x;
+        System.out.println(x + " " + y);
+        y = --x;
+        System.out.println(x + " " + y);
+        y = x--;
+        System.out.println(x + " " + y);
+    }
+}
