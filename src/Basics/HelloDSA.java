af12cdf5158fa61a567b3dbb310c89ea3a5bd808
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-10 20:09:18 +0600

    Hello DSA first commit

---
+++ src/Basics/HelloDSA.java
@@ -0,0 +1,8 @@
+package Basics;
+
+public class HelloDSA {
+    public static void main(String[] args) {
+        System.out.println("Hello DSA!");
+        System.out.println("My name is Omit.\nI am learning DSA.");
+    }
+}
