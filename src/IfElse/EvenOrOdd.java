cc632e96a9d9148e6f117ea1694c9363efe3d726
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-12 01:12:49 +0600

    even or odd first commit

---
+++ src/IfElse/EvenOrOdd.java
@@ -0,0 +1,19 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
+        } else {
+            System.out.println(num + " is an odd number.");
+        }
+
+    }
+}
