f2245bb8e11243f45cea9233adf39109e73612c0
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-12 01:17:44 +0600

    is divisible by five first commit

---
+++ src/IfElse/IsDivisibleByFive.java
@@ -0,0 +1,18 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class IsDivisibleByFive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        if (num % 5 == 0) {
+            System.out.println(num + " is divisible by 5.");
+        } else {
+            System.out.println(num + " is not divisible by 5.");
+        }
+    }
+}
