e0aa98fa284581fe206ee8258ce95f8c5001f0d6
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-13 23:54:39 +0600

    divisible by three odd numbers first commit

---
+++ src/Loops/DivisibleByThreeOddNumbers.java
@@ -0,0 +1,9 @@
+package Loops;
+
+public class DivisibleByThreeOddNumbers {
+    public static void main(String[] args) {
+        for (int i = 3; i <= 100; i = i + 6) {
+            System.out.println(i);
+        }
+    }
+}
