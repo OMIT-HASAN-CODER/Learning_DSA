c94c6d0a78ce20afe49ead05d1ecb2669f16485a
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-14 00:32:09 +0600

    arithmetic progression second commit

--- src/Loops/ArithmeticProgression.java
+++ src/Loops/ArithmeticProgression.java
@@ -6,11 +6,16 @@ public class ArithmeticProgression {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the value of n: ");
+        System.out.print("Enter the value of nth term for arithmetic progression: ");
         int n = input.nextInt();
+        System.out.print("Enter the first value for arithmetic progression: ");
+        int a = input.nextInt();
+        System.out.print("Enter common difference for arithmetic progression: ");
+        int d = input.nextInt();
 
-        for (int i = 2; i <= (3*n - 1); i += 3) {
-            System.out.println(i);
+        for (int i = 1; i <= n; i++) {
+            System.out.println(a);
+            a += d;
         }
     }
 }
