3a1d71766c6dda9fb7d4654b792a3f4c6360ab86
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-13 23:41:05 +0600

    table of n first commit

---
+++ src/Loops/TableOfN.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class TableOfN {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * n);
+        }
+    }
+}
