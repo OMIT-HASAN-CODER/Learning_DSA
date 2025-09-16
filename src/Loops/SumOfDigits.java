f0c89f213c8b89e0ba37d8f5a3960d530520fd11
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-16 23:49:39 +0600

    sum of digits second commit

--- src/Loops/SumOfDigits.java
+++ src/Loops/SumOfDigits.java
@@ -9,11 +9,15 @@ public class SumOfDigits {
         System.out.print("Enter a number: ");
         int number = input.nextInt();
 
+        //for negative number
+//        if (number < 0) {
+//            number = -number;
+//        }
         int sum = 0;
         while (number != 0) {
             sum += number % 10;
             number /= 10;
         }
-        System.out.println("The sum is: " + sum);
+        System.out.println("The sum is: " + ((sum > 0) ? sum : -sum)); //for negative sum
     }
 }
