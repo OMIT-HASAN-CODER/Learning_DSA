b26cd42a79d8ad2ab967e65240762ec6d0557259
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-17 13:26:41 +0600

    alphabet square first commit

---
+++ src/PatternPrinting/AlphabetSquare.java
@@ -0,0 +1,23 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number <= 26) {
+            for (int i = 1; i <= number; i++) {
+                for (int j = 1; j <= number; j++) {
+                    System.out.print(((char) (j + 64)) + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Invalid number");
+        }
+    }
+}
