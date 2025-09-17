a79f57b6667d423f7f3cdf6c7e9c38b800291c8e
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-17 13:47:32 +0600

    number square two first commit

---
+++ src/PatternPrinting/NumberSquareTwo.java
@@ -0,0 +1,19 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class NumberSquareTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
