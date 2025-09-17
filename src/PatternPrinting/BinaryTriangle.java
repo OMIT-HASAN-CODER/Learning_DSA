9720f961624f06e4e14b036bd9f50632ba97b2cd
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-18 01:18:38 +0600

    binary triangle first commit

---
+++ src/PatternPrinting/BinaryTriangle.java
@@ -0,0 +1,23 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class BinaryTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
