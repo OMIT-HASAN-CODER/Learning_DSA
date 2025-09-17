9f8ac9fb92efa44c7b1d5577c954735556fec81d
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-17 13:36:14 +0600

    alphabet square two first commit

---
+++ src/PatternPrinting/AlphabetSquareTwo.java
@@ -0,0 +1,23 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class AlphabetSquareTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number <= 26) {
+            for (int i = 1; i <= number; i++) {
+                for (int j = 1; j <= number; j++) {
+                    System.out.print(((char) (j + 96)) + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Invalid number");
+        }
+    }
+}
