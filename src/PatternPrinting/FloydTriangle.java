18a1769c5a97606f4550b0d217d7125c09ccff84
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-18 01:03:01 +0600

    floyd triangle first commit

---
+++ src/PatternPrinting/FloydTriangle.java
@@ -0,0 +1,21 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int a = 1;
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}
