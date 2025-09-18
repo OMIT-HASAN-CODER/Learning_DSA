aa63896157d03a06b958c161441904aff2fe4c44
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-19 01:04:15 +0600

    pyramid second commit

--- src/PatternPrinting/Pyramid.java
+++ src/PatternPrinting/Pyramid.java
@@ -9,13 +9,16 @@ public class Pyramid {
         System.out.print("Enter a number: ");
         int number = input.nextInt();
 
+        int nsp = number - 1, nst = 1;
         for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= number - i ; j++) {
+            for (int j = 1; j <=  nsp; j++) {
                 System.out.print("  ");
             }
-            for (int j = 1; j <= (2 * i) - 1; j++) {
+            for (int j = 1; j <= nst; j++) {
                 System.out.print("* ");
             }
+            nsp --;
+            nst += 2;
             System.out.println();
         }
     }
