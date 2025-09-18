3b2d00fd05cce40445322e92c1874dcf5f9f017d
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-19 00:51:29 +0600

    pyramid first commit

---
+++ src/PatternPrinting/Pyramid.java
@@ -0,0 +1,22 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner  input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number - i ; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
