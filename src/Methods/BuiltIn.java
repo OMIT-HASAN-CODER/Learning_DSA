d6edb09954b853b06dbad4d549928116fccacb10
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-20 01:03:27 +0600

    built in methods first commit

---
+++ src/Methods/BuiltIn.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class BuiltIn {
+    public static void main(String[] args) {
+        //Math
+        System.out.println(Math.sqrt(69));
+        System.out.println(Math.cbrt(69));
+        System.out.println(Math.abs(-69));
+        System.out.println(Math.floor(-6.9));
+        System.out.println(Math.ceil(-6.9));
+        System.out.println(Math.min(69, 96));
+        System.out.println(Math.max(69, 96));
+    }
+}
