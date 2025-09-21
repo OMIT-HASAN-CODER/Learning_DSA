1bcf1c34c23e0f15dbbdc6b7a629d70d614e15ad
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-22 01:20:39 +0600

    arguments second commit

--- src/Methods/Arguments.java
+++ src/Methods/Arguments.java
@@ -1,10 +1,14 @@
 package Methods;
 
 public class Arguments {
-    public static void sum(int a, int b) {
-        System.out.println(a + b);
+    public static void sum(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+    public static void max(int a, int b, int c) {
+        System.out.println(Math.max(Math.max(a, b), c));
     }
     public static void main(String[] args) {
-        sum(6, 9);
+        sum(6, 9, -3);
+        max(6, 9, -3);
     }
 }
