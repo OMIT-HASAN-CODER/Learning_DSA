ad57d950737d5f22f7c3ed4995fbded950021b6b
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-23 01:02:58 +0600

    maximum elements first commit

---
+++ src/Arrays/MaximumElements.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class MaximumElements {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter the value of index " + i + ": ");
+            arr[i] = input.nextInt();
+        }
+        System.out.println();
+
+        int max = arr[0];
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The maximum element is: " + max);
+    }
+}
