84504b32f60c89919cf0e5d137795730777c9378
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-23 23:33:51 +0600

    array of other data types

---
+++ src/Arrays/ArrayOfOtherDataTypes.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class ArrayOfOtherDataTypes {
+    public static void main(String[] args) {
+        double[] arr = new double[8];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        char[] arr2 = new char[8];
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
+
+        String[] arr3 = new String[8];
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + " ");
+        }
+    }
+}
