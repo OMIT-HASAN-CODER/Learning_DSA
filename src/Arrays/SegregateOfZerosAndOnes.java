441deeef107e4a8655a535f5c539a034472a5e49
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-29 02:44:10 +0600

    segregate of zeros and ones second commit

--- src/Arrays/SegregateOfZerosAndOnes.java
+++ src/Arrays/SegregateOfZerosAndOnes.java
@@ -2,25 +2,25 @@ package Arrays;
 
 public class SegregateOfZerosAndOnes {
     public static void main(String[] args) {
-        int[] arr = {0, 1, 1, 0, 0, 1, 0};
-        int numberOfZeros = 0;
+        int[] arr = {0, 1, 1, 0, 0, 1, 0, 1, 1, 0};
+        int n = arr.length;
 
-        for (int ele : arr) {
-            if (ele == 0) {
-                numberOfZeros ++;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            if (arr[i] == 0) {
+                i++;
+            } else if (arr[j] == 1) {
+                j--;
+            } else if (arr[i] == 1 && arr[j] == 0) {
+               arr[i] = 0;
+               arr[j] = 1;
+               i++;
+               j--;
             }
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (i < numberOfZeros) {
-                arr[i] = 0;
-            } else {
-                arr[i] = 1;
-            }
-        }
-
         for (int ele : arr) {
             System.out.print(ele + " ");
         }
+        System.out.println();
     }
 }
