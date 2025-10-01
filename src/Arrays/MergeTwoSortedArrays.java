6c4e46a2602dc200d176b63b698e6185d2f2f299
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-10-01 22:44:31 +0600

    merge two sorted arrays first commit

---
+++ src/Arrays/MergeTwoSortedArrays.java
@@ -0,0 +1,46 @@
+package Arrays;
+
+public class MergeTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] a = {2, 5, 6, 9, 20};
+        int[] b = {1, 3, 4, 5, 7, 8};
+        int[] c = new int[a.length + b.length];
+        print(c);
+        merge(c, a, b);
+        print(c);
+    }
+
+    public static void merge(int[] c, int[] a, int[] b) {
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                c[k] = a[i];
+                i++;
+            } else {
+                c[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+        if (i == a.length) {
+            while (j < b.length) {
+                c[k] = b[j];
+                j++;
+                k++;
+            }
+        } else {
+            while (i < a.length) {
+                c[k] = a[i];
+                i++;
+                k++;
+            }
+        }
+    }
+
+    public static void print(int[] c) {
+        for (int ele : c) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+}
