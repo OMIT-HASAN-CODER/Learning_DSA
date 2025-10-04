bbaa8b1a6ab7578b53d95cfe258fb1ad11d021f2
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-10-05 00:50:25 +0600

    duplicate element second commit

--- src/Complexities/DuplicateElement.java
+++ src/Complexities/DuplicateElement.java
@@ -5,12 +5,22 @@ public class DuplicateElement {
         int[] arr = {5, 1, 3, 4, 2, 4};
 
         //approach 1 --> brute force
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    System.out.println(arr[j] + " is the duplicate element.");
-                    break;
-                }
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = i + 1; j < arr.length; j++) {
+//                if (arr[i] == arr[j]) {
+//                    System.out.println(arr[j] + " is the duplicate element.");
+//                    break;
+//                }
+//            }
+//        }
+
+        //approach 2 --> marking in another array
+        boolean[] flag = new boolean[arr.length + 1];
+        for (int ele : arr) {
+            if (flag[ele]) {
+                System.out.println(ele + " is the duplicate element.");
+            } else {
+                flag[ele] = true;
             }
         }
     }
